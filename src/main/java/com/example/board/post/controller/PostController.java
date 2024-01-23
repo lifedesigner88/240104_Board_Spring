cf2d8caa7854a33df2f0e82e0972027d05b4a1cb
lifedesigner88@gmail.com <lifedesigner88@gmail.com>
2024-01-23 16:29:06 +0900

    2024.01.23 7th

--- src/main/java/com/example/board/post/controller/PostController.java
+++ src/main/java/com/example/board/post/controller/PostController.java
@@ -2,16 +2,17 @@ package com.example.board.post.controller;
 
 import com.example.board.post.domain.Post;
 import com.example.board.post.dto.PostCreateReqDto;
-import com.example.board.post.dto.PostDetailResDto;
-import com.example.board.post.dto.PostListResDto;
 import com.example.board.post.dto.PostUpdateReqDto;
 import com.example.board.post.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
-@RestController
+@Controller
 @RequestMapping("post")
 public class PostController {
 
@@ -21,36 +22,43 @@ public class PostController {
     }
 
 //    Create
-//    @GetMapping("create")
-
+    @GetMapping("create")
+    public String postCreateForm(){
+        return "post/post-create";
+    }
 
     @PostMapping("create")
-    public Post createPost(PostCreateReqDto dto) {
-        return service.create(dto);
+    public String createPost(PostCreateReqDto dto) {
+        Post post = service.create(dto);
+        return "redirect:/post/detail/" + post.getId();
     }
 
 //    Read
     @GetMapping("list")
-    public List<PostListResDto> getAllPosts() {
-        return service.getAllPosts();
+    public String getAllPosts(Model model) {
+        model.addAttribute("posts", service.getAllPosts());
+        return "post/post-list";
     }
 
     @GetMapping("detail/{id}")
-    public PostDetailResDto getPostDetail(@PathVariable Long id) {
-        return service.getPostDetail(id);
+    public String getPostDetail(@PathVariable Long id, Model model) {
+        model.addAttribute("post", service.getPostDetail(id));
+        return "post/post-detail";
     }
 
 //    Update
     @PostMapping("update/{id}")
-    public Post updatePost(@PathVariable Long id, PostUpdateReqDto dto) {
-        return service.update(id, dto);
+    public String updatePost(@PathVariable Long id, PostUpdateReqDto dto) {
+        Post post =  service.update(id, dto);
+        return "redirect:/post/detail/" + post.getId();
+
     }
 
 //    Delete
     @GetMapping("delete/{id}")
     public String deletePost(@PathVariable Long id) {
         service.deletePost(id);
-        return "OK";
+        return "redirect:/post/list";
     }
 
 }
