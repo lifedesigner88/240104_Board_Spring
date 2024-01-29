db8f5902872a2f1b4b6b4a53cdfd4e4ec6f4bdfa
lifedesigner88@gmail.com <lifedesigner88@gmail.com>
2024-01-29 15:50:39 +0900

    2024.01.29 6th 비포, 에프터, 어라운드 관련 내용

--- src/main/java/com/example/board/common/AopLogService.java
+++ src/main/java/com/example/board/common/AopLogService.java
@@ -1,9 +1,17 @@
 package com.example.board.common;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @Aspect
 @Component
@@ -13,22 +21,22 @@ public class AopLogService {
 //    @Pointcut(value = "execution(* com.example.board..controller..*.*(..))")
     @Pointcut("within(@org.springframework.stereotype.Controller *)")
     public void controllerPointCut(){
-
     }
-
-
-//    어드바이스들
 
 
 
     @Before("controllerPointCut()")
-    public void beforeController(){
+    public void beforeController(JoinPoint joinPoint){
         log.info("[ --  Before executing controller method]");
 //        메서드가 실행되기 전에 인증, 입력값 겆읃응을 수행하는 용도로 사용하는 단계
+
+        ServletRequestAttributes servletReq =
+                (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        ObjectNode objectNode = getJsonNodes(joinPoint, servletReq);
+        log.info(objectNode.toString());
+
+
     }
-
-
-
 
     @After("controllerPointCut()")
     public void afterController(){
@@ -37,19 +45,57 @@ public class AopLogService {
 
 
 
-
+//    어드바이스.
     @Around("controllerPointCut()")
 //    join point란 aop 대상으로 하는 컨트롤러의 특정 메스드를 의미
-    public Object controllerLogger(ProceedingJoinPoint proceedingJoinPoint){
+    public Object controllerLogger(ProceedingJoinPoint joinPoint){
 
-        log.info("[ --  request method" + proceedingJoinPoint.getSignature().toString());
+//        log.info("[ --  request method" + joinPoint.getSignature().toString());
+        ServletRequestAttributes servletReq =
+                (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        ObjectNode objectNode = getJsonNodes(joinPoint, servletReq);
+        log.info(objectNode.toString());
+
         try {
 //            본래의 컨트롤러 매서드 호출하는 부분.
-            return proceedingJoinPoint.proceed();
+            return joinPoint.proceed();
         } catch (Throwable e) {
             log.error(e.getMessage());
             throw new RuntimeException(e);
         }
     }
+
+
+
+    private ObjectNode getJsonNodes(ProceedingJoinPoint joinPoint, ServletRequestAttributes servletReq) {
+        HttpServletRequest req = servletReq.getRequest();
+
+//        Json 형태로 사용자의 요청을 조립하기 위한 로직
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectNode objectNode = objectMapper.createObjectNode();
+
+        objectNode.put("Method Name", joinPoint.getSignature().getName());
+        objectNode.put("CRUD Name", req.getMethod());
+        Map<String, String[]> paramMap = req.getParameterMap();
+        ObjectNode objectNodeDetail = objectMapper.valueToTree((paramMap));
+        objectNode.set("user inputs", objectNodeDetail);
+        return objectNode;
+    }
+
+    private ObjectNode getJsonNodes(JoinPoint joinPoint, ServletRequestAttributes servletReq) {
+        HttpServletRequest req = servletReq.getRequest();
+
+//        Json 형태로 사용자의 요청을 조립하기 위한 로직
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectNode objectNode = objectMapper.createObjectNode();
+
+        objectNode.put("Method Name", joinPoint.getSignature().getName());
+        objectNode.put("CRUD Name", req.getMethod());
+        Map<String, String[]> paramMap = req.getParameterMap();
+        ObjectNode objectNodeDetail = objectMapper.valueToTree((paramMap));
+        objectNode.set("user inputs", objectNodeDetail);
+        return objectNode;
+    }
+
 
 }
